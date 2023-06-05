684a08fd58ffcab626b1cc88be859486a6f7ab99
Leticia-Guimaraes <124922074+Leticia-Guimaraes@users.noreply.github.com>
2023-06-05 13:16:30 -0300

    Correções

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,27 +1,30 @@
-import "./style.css"
-import { AiOutlineShoppingCart , AiOutlineUser } from 'react-icons/ai'
-import { BsFilterRight } from 'react-icons/bs'
+import "./style.css";
+import { AiOutlineShoppingCart, AiOutlineUser } from "react-icons/ai";
+import { BsFilterRight } from "react-icons/bs";
 import Logo from "../Logo/Logo";
+import { NavLink } from "react-router-dom";
 
 const Header = () => {
   return (
     <header className="container">
-  
-        <Logo />
+      <Logo />
 
-          <ul className="homeUl">
-            <li>Home</li>
-            <li>Products</li>
-            <li>Contacts</li>
-          </ul>
-          
-        <div className="headerIcons">
-          <AiOutlineShoppingCart />
-          <AiOutlineUser />
-            <div id="currency"></div>
-          <BsFilterRight />
-        </div>
-    
+      <ul className="homeUl">
+        <NavLink className="navLink" to={"/"}>
+          <li>Home</li>
+        </NavLink>
+        <NavLink className="navLink" to={"/products"}>
+          <li>Products</li>
+        </NavLink>
+        <li>Contacts</li>
+      </ul>
+
+      <div className="headerIcons">
+        <AiOutlineShoppingCart />
+        <AiOutlineUser />
+        <div id="currency"></div>
+        <BsFilterRight />
+      </div>
     </header>
   );
 };
